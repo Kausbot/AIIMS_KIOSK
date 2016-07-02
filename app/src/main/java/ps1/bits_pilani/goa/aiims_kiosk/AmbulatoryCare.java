25e4a333e17e8f2c36b043709dc1743e0b13ff94
soumya03 <f2014664@goa.bits-pilani.ac.in>
2016-07-02 23:00:44 +0530

    AmbulatoryCare completed, scroll added

--- app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/AmbulatoryCare.java
+++ app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/AmbulatoryCare.java
@@ -2,12 +2,17 @@ package ps1.bits_pilani.goa.aiims_kiosk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
 
 public class AmbulatoryCare extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView textview= (TextView) findViewById(R.id.your_textview_id);
+        textview.setMovementMethod(new ScrollingMovementMethod());
         setContentView(R.layout.activity_ambulatory_care);
     }
 }
