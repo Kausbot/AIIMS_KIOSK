15c1bfda6176754054944b85890105e52c12f23b
Kausbot <f2014420@goa.bits-pilani.ac.in>
2016-07-02 17:37:05 +0530

    malaria04 added

---
+++ app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/Malaria04.java
@@ -0,0 +1,13 @@
+package ps1.bits_pilani.goa.aiims_kiosk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Malaria04 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_malaria04);
+    }
+}
