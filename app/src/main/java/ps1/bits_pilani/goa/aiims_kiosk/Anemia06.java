087ca34a2862b74873b98962f60ba701c28be58c
Kausbot <f2014420@goa.bits-pilani.ac.in>
2016-07-04 14:54:45 +0530

    Disease prevention home buttons linked

--- app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/Anemia06.java
+++ app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/Anemia06.java
@@ -1,7 +1,9 @@
 package ps1.bits_pilani.goa.aiims_kiosk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Anemia06 extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class Anemia06 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anemia06);
+
+
     }
 }
+
