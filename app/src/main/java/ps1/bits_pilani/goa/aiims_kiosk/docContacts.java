fa07e756acf3c6fbbfab02cf13ebb21d04ac8a6b
Cazaimi <anmol01shukla@gmail.com>
2016-07-04 17:40:40 +0530

    Added the following:
    1. Added and completed the doctor's contact activity.
    Todo: add material design
    
    :)

--- app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/docContacts.java
+++ app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/docContacts.java
@@ -1,49 +1,19 @@
 package ps1.bits_pilani.goa.aiims_kiosk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+public class docContacts extends Activity {
 
-public class docContacts extends AppCompatActivity {
-
-    private ListView mainListView ;
-    private ArrayAdapter<String> listAdapter ;
-
-
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc_contacts);
 
-        // Find the ListView resource.
-         mainListView = (ListView) findViewById( R.id.doctorContacts );
-
-        // Create and populate a List of planet names.
-        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
-                "Jupiter", "Saturn", "Uranus", "Neptune"};
-        ArrayList<String> planetList = new ArrayList<String>();
-        planetList.addAll( Arrays.asList(planets) );
-
-        // Create ArrayAdapter using the planet list.
 
 
-        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
-
-        // Add more planets. If you passed a String[] instead of a List<String>
-        // into the ArrayAdapter constructor, you must not add more items.
-        // Otherwise an exception will occur.
-        listAdapter.add( "Ceres" );
-        listAdapter.add( "Pluto" );
-        listAdapter.add( "Haumea" );
-        listAdapter.add( "Makemake" );
-        listAdapter.add( "Eris" );
-
-        // Set the ArrayAdapter as the ListView's adapter.
-        mainListView.setAdapter( listAdapter );
     }
 }
-
