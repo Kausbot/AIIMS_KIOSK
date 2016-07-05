1d14abd395f0484a01ddcad873467664e60f4bf2
Cazaimi <anmol01shukla@gmail.com>
2016-07-05 15:31:19 +0530

    Added the following:
    1. Added intents in MainMenu
    2. Added class called Maps
    3. Added activity called maps
    :)

--- app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/Maps.java
+++ app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/Maps.java
@@ -1,7 +1,28 @@
 package ps1.bits_pilani.goa.aiims_kiosk;
 
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 /**
  * Created by lemonade on 5/7/16.
  */
-public class Maps {
+public class Maps extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+
+    }
+
+
 }
