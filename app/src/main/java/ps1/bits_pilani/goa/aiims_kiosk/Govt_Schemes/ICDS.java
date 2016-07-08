a562f89c52139025233096d3375b6a8c2f7e8d1b
Cazaimi <anmol01shukla@gmail.com>
2016-07-08 20:09:31 +0530

    Added the following:
    1. Added the schemes intents in schemes.java
    2. Added the classes concerning the classes.
    3. Finished IGMSY
    
    :)

---
+++ app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/Govt_Schemes/ICDS.java
@@ -0,0 +1,15 @@
+package ps1.bits_pilani.goa.aiims_kiosk.Govt_Schemes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ps1.bits_pilani.goa.aiims_kiosk.R;
+
+public class ICDS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icds);
+    }
+}
