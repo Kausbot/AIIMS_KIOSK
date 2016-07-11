712f0feb0893bf8f2e5da7e57de5c4970bf1420d
Cazaimi <anmol01shukla@gmail.com>
2016-07-12 00:43:47 +0530

    REctificatons

--- app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/MedicalServices.java
+++ app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/MedicalServices.java
@@ -27,6 +27,27 @@ public class MedicalServices extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.home) {
+
+            Intent intent = new Intent(this, MainMenu.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            return true;
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     public void ambcare (View view) {
         Intent i = new Intent(this,AmbulatoryCare.class);
