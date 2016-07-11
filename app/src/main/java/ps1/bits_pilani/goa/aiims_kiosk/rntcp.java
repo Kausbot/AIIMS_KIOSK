417b6d8ac055828681556e4b72bb4fdca3af626d
soumya03 <f2014664@goa.bits-pilani.ac.in>
2016-07-12 01:24:02 +0530

    govt schemes home button added

--- app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/rntcp.java
+++ app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/rntcp.java
@@ -1,7 +1,12 @@
 package ps1.bits_pilani.goa.aiims_kiosk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import ps1.bits_pilani.goa.aiims_kiosk.Govt_Schemes.IGMSY;
+import ps1.bits_pilani.goa.aiims_kiosk.Govt_Schemes.NFSM;
 
 public class rntcp extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class rntcp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rntcp);
+    }
+    public void rntcpp (View view) {
+        Intent i = new Intent(this,NFSM.class);
+        startActivity(i);
+    }
+    public void rntcpn (View view) {
+        Intent i = new Intent(this,IGMSY.class);
+        startActivity(i);
+    }
+    public void rntcph (View view) {
+        Intent i = new Intent(this,Schemes.class);
+        startActivity(i);
     }
 }
