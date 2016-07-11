121ea9fcf12632327bfdda0318d0b24c96cb63a9
soumya03 <f2014664@goa.bits-pilani.ac.in>
2016-07-11 16:53:14 +0530

    medical services

--- app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/MedicalServices.java
+++ app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/MedicalServices.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MedicalServices extends AppCompatActivity {
 
@@ -27,7 +28,34 @@ public class MedicalServices extends AppCompatActivity {
         return true;
     }
 
-    @Override
+    public void ambcare (View view) {
+        Intent i = new Intent(this,AmbulatoryCare.class);
+        startActivity(i);
+    }
+
+    public void famcare (View view) {
+        Intent i = new Intent(this,OutReachFamilyCare.class);
+        startActivity(i);
+    }
+    public void ambcare (View view) {
+        Intent i = new Intent(this,AmbulatoryCare.class);
+        startActivity(i);
+    }
+    public void acute (View view) {
+        Intent i = new Intent(this,AcuteCar.class);
+        startActivity(i);
+    }
+    public void long (View view) {
+        Intent i = new Intent(this,LongTermChronicCare.class);
+        startActivity(i);
+    }
+    public void surgical (View view) {
+        Intent i = new Intent(this,SurgicalOperations.class);
+        startActivity(i);
+    }
+}
+
+   /* @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -47,4 +75,4 @@ public class MedicalServices extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-}
+}*/
--- app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/MedicalTechnology.java
+++ app/src/main/java/ps1/bits_pilani/goa/aiims_kiosk/MedicalTechnology.java
@@ -1,9 +1,11 @@
 package ps1.bits_pilani.goa.aiims_kiosk;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MedicalTechnology extends AppCompatActivity {
 
@@ -14,5 +16,16 @@ public class MedicalTechnology extends AppCompatActivity {
         String color="#b91da4";
         ActionBar bar = getActionBar();
         bar.setBackgroundDrawable(new ColorDrawable());
+
+
     }
-}
+    public void pathology (View view) {
+        Intent i = new Intent(this,PathologyAndLabMedicine.class);
+        startActivity(i);
+    }
+    public void ambcare (View view) {
+        Intent i = new Intent(this,AmbulatoryCare.class);
+        startActivity(i);
+    }
+    }
+
